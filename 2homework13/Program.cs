6308f795c1ddabc29249b44a1adf49053302861e
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:18:24 +0500

    zadach

---
+++ 2homework13/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+if (num / 100 == 0)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    while (num != 0)
+    {   
+        if (num / 100 < 10)
+        {
+        Console.WriteLine(num % 10);
+        break;
+        }
+        num = num / 10;
+    }
+}
+
---
+++ 2homework15/Program.cs
@@ -0,0 +1,10 @@
+﻿int day = int.Parse(Console.ReadLine());
+if (day <= 5)
+{
+    Console.WriteLine($"{day}->нет");
+}
+else
+{
+    Console.WriteLine($"{day}->да");
+}
+
